bb9f523a1c4936e5aa4f64dc5afb984a8bf12148
iprincewang <>
2018-09-07 16:59:27 +0800

    资源文件

---
+++ myblog/themes/next/source/dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "PDD洪荒之力",
+        artist: '徐梦圆',
+        url: '../music/花粥,马雨阳 - 盗将行.flac',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+      },
+    ]
+});
